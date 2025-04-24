539ffcf2a64f6287737849b185e139f8352d8042
Oscar Gaidos <ogaidos@gmail.com>
2025-04-24 20:14:41 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -0,0 +1,97 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+
+@Entity
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	//como este eh um obj auxiliar tem que ser instanciado
+	//Id embutido em um dados auxiliar
+	@EmbeddedId 
+	private ItemPedidoPK id = new ItemPedidoPK();
+	
+	private Double desconto;
+	private Integer quantidade;
+	private Double preço;
+	
+	//isto tera associado uma chave primaria (PK), que è uma ref para o pedido e o 
+	//produto
+	
+	public ItemPedido(){
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preço) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.desconto = desconto;
+		this.quantidade = quantidade;
+		this.preço = preço;
+	}
+	
+	public Pedido getPedido(){
+		return id.getPedido();
+	}
+	
+	public Produto getProduto(){
+		return id.getProduto();
+	}
+
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+}
