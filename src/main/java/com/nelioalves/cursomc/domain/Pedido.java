7f53dd3f2de2069a66ad7ad6e27777768cf313e8
Oscar Gaidos <ogaidos@gmail.com>
2025-05-01 21:19:18 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -30,11 +30,10 @@ public class Pedido implements Serializable{
 	@JsonFormat(pattern="dd/MM/yyyy hh:mm")
 	private Date instante;
 	
-	@JsonManagedReference
+	@JsonIgnore
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
